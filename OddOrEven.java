afce52141b65983435acb69ea0d2b74d9338f25f
Aparajitaparija <139795100+Aparajitaparija@users.noreply.github.com>
2023-07-18 23:29:04 +0530

    Add files via upload

---
+++ OddOrEven.java
@@ -0,0 +1,21 @@
+package week1.day2;
+
+public class OddOrEven {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	int num = 35;
+	
+	if(num%2== 0) {
+		System.out.println("The number "+num+ " is an even number.");
+	
+	}
+	else {
+		System.out.println("The number "+num+ " is an odd number");
+		}
+		
+		
+	}
+
+}
---
+++ negativeToPositive.java
@@ -0,0 +1,23 @@
+package week1.day2;
+
+public class negativeToPositive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a = -21;
+		if(a<0) {
+			System.out.println("The given number "+a+ " is a negative number.");
+		}
+			else{
+			System.out.println("The given number "+a+ " is a positive number.");
+		}
+		if(a<0) {
+			System.out.println(-a);
+		}
+		else{
+			System.out.println(a);
+	}
+
+	}
+}
